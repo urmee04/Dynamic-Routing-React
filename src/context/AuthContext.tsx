84875ee9533f02ab657f42d8a6565da4eaf5c62a
urmee04 <urmee04sust@gmail.com>
2025-08-13 09:44:47 -0400

    refactor code to fix issues

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,3 @@
-//Authentication context will manage and provide the user's authentication state throughout the application
-
 import { createContext, useState, useContext, type ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -12,7 +10,9 @@ interface AuthContextType {
 
 //create the context with a default undefined value
 // A runtime check will prevent using it without a provider
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
 
 //define the props for the provider component
 interface AuthProviderProps {
@@ -22,11 +22,11 @@ interface AuthProviderProps {
 
 //Create the AuthProvider component
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  // State to track if the user is authenticated
+  // state to track if the user is authenticated
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
 
-  //function to simulate user login
+  // Function to simulate user login
   const login = () => {
     setIsAuthenticated(true);
     //redirect to the admin page after logging in
