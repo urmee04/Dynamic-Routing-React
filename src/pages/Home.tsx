0989bc43ca90a3cf64d2ebbb70fa488bae4dd651
urmee04 <urmee04sust@gmail.com>
2025-08-13 10:02:42 -0400

    create Home page component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="container mx-auto p-8 text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Blog App</h1>
+      <p className="text-lg text-gray-700">
+        Log in to access the admin dashboard!!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
