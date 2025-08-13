96518e6026487b06c3f3980c7c9808c38e46a61d
urmee04 <urmee04sust@gmail.com>
2025-08-13 10:16:36 -0400

    create protected Admin page

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,12 @@
+const Admin = () => {
+  return (
+    <div className="container mx-auto p-8 text-center">
+      <h1 className="text-4xl font-bold text-green-700 mb-4">
+        Admin Dashboard
+      </h1>
+      <p className="text-lg text-gray-800">You’re signed in!</p>
+    </div>
+  );
+};
+
+export default Admin;
