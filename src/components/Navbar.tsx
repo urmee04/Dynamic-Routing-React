58dd7fd040a8c3d4a39663db92bbe57d0bc0e6b5
urmee04 <urmee04sust@gmail.com>
2025-08-13 10:00:14 -0400

    change Navbar bg color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="bg-gray-800 text-white p-4 shadow-md">
+    <header className="bg-orange-400 text-white p-4 shadow-md">
       <nav className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
